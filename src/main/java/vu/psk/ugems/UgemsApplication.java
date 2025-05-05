dffcdbb918b006df22ecc4544aeb19759b416d31
dovix <kavoliunudovydas@gmail.com>
2025-05-05 23:30:37 +0300

    init comit

---
+++ src/main/java/vu/psk/ugems/UgemsApplication.java
@@ -0,0 +1,13 @@
+package vu.psk.ugems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UgemsApplication.class, args);
+    }
+
+}
