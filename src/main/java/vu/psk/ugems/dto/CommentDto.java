c1ac8884e079c32ca83fd37a4980379b55ea5145
mantvydasMac <144117211+mantvydasMac@users.noreply.github.com>
2025-05-12 14:00:53 +0300

    CommentDTO ???

--- src/main/java/vu/psk/ugems/dto/CommentDto.java
+++ src/main/java/vu/psk/ugems/dto/CommentDto.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 
 @Data
 @NoArgsConstructor
@@ -12,7 +13,7 @@ import java.time.LocalDate;
 public class CommentDTO {
     private Long id;
     private String content;
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
     private Long taskId;
     private Long profileId;
 }
