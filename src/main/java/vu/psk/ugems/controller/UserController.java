2f8cf893bde5c17dac1d468abf7d0a9457970eb7
Jokubas Viliusis <viliusisj@gmail.com>
2025-05-17 13:29:01 +0300

    User authentication implementation start

--- src/main/java/vu/psk/ugems/controller/UserController.java
+++ src/main/java/vu/psk/ugems/controller/UserController.java
@@ -1,21 +1,21 @@
-package vu.psk.ugems.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import vu.psk.ugems.dto.UserDTO;
-import vu.psk.ugems.entity.User;
-import vu.psk.ugems.service.UserService;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/v1/users")
-public class UserController {
-    private final UserService userService;
-    @PostMapping
-    public UserDTO registerUser(@RequestBody UserDTO userDTO) {
-
-    }
-}
+//package vu.psk.ugems.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import vu.psk.ugems.dto.UserDTO;
+//import vu.psk.ugems.entity.User;
+//import vu.psk.ugems.service.UserService;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/v1/users")
+//public class UserController {
+//    private final UserService userService;
+//    @PostMapping
+//    public UserDTO registerUser(@RequestBody UserDTO userDTO) {
+//
+//    }
+//}
