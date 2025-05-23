0319b48973b02bf50748782bde759a7685615dbe
migle-kirilovaite <migle.kirilovaite@gmail.com>
2025-05-23 13:51:37 +0300

    Cross-cutting functionality???

--- src/main/java/vu/psk/ugems/interceptor/LoggingAspect.java
+++ src/main/java/vu/psk/ugems/interceptor/LoggingAspect.java
@@ -3,6 +3,7 @@ package vu.psk.ugems.interceptor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Aspect
+@ConditionalOnProperty(prefix = "business.logging", name = "enabled", havingValue = "true", matchIfMissing = true)
 @Component
 public class LoggingAspect {
 
@@ -30,14 +32,12 @@ public class LoggingAspect {
                 ? authentication.getName()
                 : "anonymous";
 
-        String authorities = authentication.getAuthorities().toString();
+        String authorities = (authentication != null && authentication.getAuthorities() != null)
+                ? authentication.getAuthorities().toString()
+                : "[]";
 
         String logMessage = String.format(
-                "Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): %s\n" +
-                        "User: %s\n" +
-                        "Roles: %s\n" +
-                        "Method class: %s\n" +
-                        "Method name: %s\n",
+                "Time: %s | User: %s | Roles: %s | Class: %s | Method: %s",
                 timestamp,
                 username,
                 authorities,
